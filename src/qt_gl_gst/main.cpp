06378bec3648b848f989ebf23f09455d113034f9
Ed Langley <elangley@mpcdata.com>
2013-03-01 10:01:08 +0000

    Tidy up main and MainWindow
    Remove left over commented out code after adding ControlForm.

--- src/qt_gl_gst/main.cpp
+++ src/qt_gl_gst/main.cpp
@@ -1,37 +1,13 @@
 #include <QApplication>
-//#include "glwidget.h"
 #include "mainwindow.h"
-
-/*
-#include <QMainWindow>
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    MainWindow() {};
-    MainWindow(int argc, char *argv[], QWidget *parent = 0);
-
-private:
-};
-
-MainWindow::MainWindow(int argc, char *argv[], QWidget *parent)
-{
-    GLWidget *widget = new GLWidget(argc, argv);
-
-    setCentralWidget(widget);
-}
-*/
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //GLWidget widget(argc, argv);
-    //widget.show();
-    //widget.showFullScreen();
 
     MainWindow mainWindow(argc, argv);
-    mainWindow.show();
+    //mainWindow.show();
+    mainWindow.showFullScreen();
 
     return a.exec();
 }
